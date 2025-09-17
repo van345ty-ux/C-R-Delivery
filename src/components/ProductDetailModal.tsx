18f1c3636f4ec63bf250a2b83b72669fbe12f824
[dyad] <van345ty@gmail.com>
2025-09-17 18:42:45 -0300

    Updated src/components/ProductDetailModal.tsx

--- src/components/ProductDetailModal.tsx
+++ src/components/ProductDetailModal.tsx
@@ -99,7 +99,7 @@ export const ProductDetailModal: React.FC<ProductDetailModalProps> = ({
                 onClick={() => handleQuantityChange(1)}
                 className="bg-gray-200 hover:bg-gray-300 rounded-full p-2"
               >
-                <Plus className="w-0 h-4" />
+                <Plus className="w-4 h-4" />
               </button>
             </div>
             <Button
