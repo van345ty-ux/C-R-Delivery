3abf710749b11844c4589fc17838fa7739dcefbe
[dyad] <van345ty@gmail.com>
2025-09-17 10:40:15 -0300

    [dyad] Corrigindo erro de sintaxe em HomePage.tsx - wrote 1 file(s)

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -181,7 +181,7 @@ export const HomePage: React.FC<HomePageProps> = ({
           onClose={handleClosePromotionModal}
           title={promotionModalTitle}
           onViewPromotion={handleViewPromotion}
-          onAddToCart={handleAddToCart} {/* Passando onAddToCart aqui */}
+          onAddToCart={handleAddToCart} 
         />
       )}
       <Footer /> {/* Adicionando o Footer aqui */}
