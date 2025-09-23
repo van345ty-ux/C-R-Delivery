7c6c228b59ff4a6718c4e542d37aaf087e489772
[dyad] <van345ty@gmail.com>
2025-09-23 17:04:04 -0300

    [dyad] Ajustando texto no rodapé - wrote 1 file(s)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer: React.FC = () => {
     <footer className="bg-red-600 text-white py-6 px-4 sm:px-6 lg:px-8 mt-8">
       <div className="max-w-7xl mx-auto flex flex-col sm:flex-row justify-between items-center text-center sm:text-left">
         <p className="text-sm mb-2 sm:mb-0">
-          Copyright © 2025 C&R Sushi, Todos os direitos reservados.
+          Copyright © 2025 - C&R Sushi, Todos os direitos reservados.
         </p>
         <div className="text-xs text-right">
           <a href="#" className="hover:underline mr-2">Política de privacidade</a>
