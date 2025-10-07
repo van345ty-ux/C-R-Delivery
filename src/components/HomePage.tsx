844275aec050f3624654ddf5d1d32848185253cd
[dyad] <van345ty@gmail.com>
2025-10-07 15:41:47 -0300

    Updated src/components/HomePage.tsx

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -136,7 +136,7 @@ export const HomePage: React.FC<HomePageProps> = ({
   };
 
   const handleViewPromotion = () => {
-    setMenuFilter('Promções');
+    setMenuFilter('Promo');
     setShowPromotions(false);
     // Esta ação (Ver no Cardápio) não acionará a notificação de cupom, conforme solicitado.
   };
